975b5d697b183d69ddf40d5d93faaa3e6be5ef46
DENNYYESSAR <dennisokeri5@gmail.com>
2024-08-21 22:59:19 +0300

    MRU Caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -2,51 +2,48 @@
 """
 MRUCache module
 """
-
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
     """
-    MRUCache implements a caching system with a Most Recently Used (MRU)
-    eviction policy.
+    Represents a Most Recently Used (MRU) cache.
+    Inherits from the BaseCaching class.
     """
-
     def __init__(self):
         """
-        Initialize the MRU cache system.
+        Initializes the MRUCache instance.
+        Calls the parent class (BaseCaching) constructor.
         """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.mru_order = []
 
     def put(self, key, item):
         """
-        Add an item in the cache.
-
-        If either key or item is None, do nothing.
-        If the cache exceeds the MAX_ITEMS limit, discard the most recently
-        used item.
+        Adds an item to the cache.
+        Discards the most recently used item if the cache size exceeds the
+        maximum.
         """
         if key is None or item is None:
             return
 
-        if key in self.cache_data:
-            self.cache_data.move_to_end(key)
         self.cache_data[key] = item
+        if key in self.mru_order:
+            self.mru_order.remove(key)
+        self.mru_order.append(key)
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discarded_key, _ = self.cache_data.popitem(last=True)
+            discarded_key = self.mru_order.pop(0)
             print(f"DISCARD: {discarded_key}")
+            del self.cache_data[discarded_key]
 
     def get(self, key):
         """
-        Get an item by key from the cache.
-
-        Return None if the key is None or doesn't exist in the cache.
+        Retrieves an item from the cache.
         """
         if key is None or key not in self.cache_data:
             return None
 
-        self.cache_data.move_to_end(key)
+        self.mru_order.remove(key)
+        self.mru_order.append(key)
         return self.cache_data[key]
