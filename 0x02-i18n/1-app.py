bb4558bbdcd5511f4d8e8559dd60837e5afe4348
DENNYYESSAR <dennisokeri5@gmail.com>
2024-08-28 00:09:53 +0300

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Flask app with Babel for internationalization support.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Config class to set up the available languages and default settings.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index() -> str:
+    """
+    Handles the root route by rendering an index.html template.
+
+    Returns:
+        str: The rendered HTML content for the root page.
+    """
+    return render_template('1-index.html')
